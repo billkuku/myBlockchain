31f2d45d9a5c087b6d62d083387bba6a6df2b32d
billkuku <49456914+billkuku@users.noreply.github.com>
2020-07-02 15:23:04 +0200

    Update VerifyBlock.java

--- Kafka_Blockchain_3/src/myBlockchain/VerifyBlock.java
+++ Kafka_Blockchain_3/src/myBlockchain/VerifyBlock.java
@@ -1,5 +1,5 @@
 package myBlockchain;
-/** this method is used for verifying block from distributed-block-topic by verifier-nodes.
+/** this class is used for verifying block from distributed-block-topic by verifier-nodes.
  * get block from distributed-block-topic and verify it, save new hash locally 
  * in visualledger-file, if validating fail, log the block in log-file
  * 
@@ -11,9 +11,6 @@ import java.security.NoSuchAlgorithmException;
 
 import Utilities.WriteToFile;
 
-
-/*get block distributed-block-topic and verify it, save new hash locally, if validating fail, log the block*/
-
 public class VerifyBlock {
 	public static String blockVerify(String block, String lprevioushash) throws IOException, NoSuchAlgorithmException{
 		//distruct block in hash, prehash,ts, and transaction
@@ -23,26 +20,27 @@ public class VerifyBlock {
 		String btimeStamp = block.substring(128,141);
 		String bsourceData = block.substring(141);
 		
-		/*System.out.println("block hash:" + bhash);
-		System.out.println("block prehash:" + bpreviousHash);
-		System.out.println("block sourcedata:" + bsourceData);
-		System.out.println("local timestamp:" + btimeStamp);*/
 		//initiat a new calculated prehash in verify-process
 		String lhash = null;
 
-		//prepare the data to calculate hash
-        String dataToHash = lprevioushash + btimeStamp + bsourceData;
-        //hash data use SHA256
+		/** prepare the data and calculate hash
+		*/
+        	String dataToHash = lprevioushash + btimeStamp + bsourceData;
+        	//hash data use SHA256
 		byte[] byteSource= dataToHash.getBytes();
-    	MessageDigest digest = MessageDigest.getInstance("SHA-256");
+    		MessageDigest digest = MessageDigest.getInstance("SHA-256");
 		byte[] encodedhash = digest.digest(byteSource);
-		//transfer hash from byte[] to String
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i< encodedhash.length ;i++){
-            sb.append(Integer.toString((encodedhash[i] & 0xff) + 0x100, 16).substring(1));
-        }
-        lhash = sb.toString();
-		//verify hash and write new hash to local ledger
+		
+		/** transfer hash from byte[] to String
+		*/
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i< encodedhash.length ;i++){
+		    sb.append(Integer.toString((encodedhash[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		lhash = sb.toString();
+		
+		/**verify hash through comparing lhash and bhash, if faild block will be add to log-file.
+		*/
 		if(!lhash.equals(bhash)){
 			WriteToFile.appendToFile("log", block + " verify failed!");
 			System.out.println("verify failed! Related Block data was saved in log-file");
