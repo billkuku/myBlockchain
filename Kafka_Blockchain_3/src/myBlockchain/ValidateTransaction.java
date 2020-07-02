7380973329e57810fc81bb857d8478bef3e5df4c
billkuku <49456914+billkuku@users.noreply.github.com>
2020-07-02 15:17:14 +0200

    Update ValidateTransaction.java

--- Kafka_Blockchain_3/src/myBlockchain/ValidateTransaction.java
+++ Kafka_Blockchain_3/src/myBlockchain/ValidateTransaction.java
@@ -1,6 +1,7 @@
 package myBlockchain;
 
-/** this method is used for validating transaction from transaction-topic by validate-node.
+/** this class is used for validating transaction from transaction-topic by validate-node.
+ * it is integrated in AppValidator.
  * 
  * @author beier
  */
@@ -13,28 +14,35 @@ import Utilities.WriteToFile;
 //official vision of block creater, source and previoushash get from topic
 public class ValidateTransaction {
 		public static String createBlock(String source, String previousHash) throws NoSuchAlgorithmException, IOException {			
-				//initialize the block data
+				/** initialize the block, create the var in block.
+				*/
 				String hash = null;
-				//create data to hash
 				long timeStamp = new Date().getTime();
 				String strTimeStamp = Long.toString(timeStamp); 
-		        String dataToHash = previousHash + strTimeStamp + source;
+		        	String dataToHash = previousHash + strTimeStamp + source;
 		        
-		        //The SHA-256 algorithm generates 256-bit hash. This is a one-way function, so the result cannot be decrypted back to the original value.
+		       		/**The SHA-256 algorithm generates 256-bit hash. This is a one-way function, 
+				 * so the result cannot be decrypted back to the original value.
+				 */
 				byte[] byteSource= dataToHash.getBytes();
-		    	MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		    		MessageDigest digest = MessageDigest.getInstance("SHA-256");
 				byte[] encodedhash = digest.digest(byteSource);
-				//transfer hashcode to String
-		        StringBuilder sb = new StringBuilder();
-		        for(int i=0; i< encodedhash.length ;i++)
-		        {
-		            sb.append(Integer.toString((encodedhash[i] & 0xff) + 0x100, 16).substring(1));
-		        }
-		        hash = sb.toString();
-		        WriteToFile.insert("visualledger", hash);
-		        String block = hash + dataToHash;
-		        //block sequence is: hash0-64, previousHash65-128,Timestamp129-141,source142-
-		        System.out.println(block);
-		        return block;
+			
+				/** transfer hashcode to String
+				*/
+		      		StringBuilder sb = new StringBuilder();
+				for(int i=0; i< encodedhash.length ;i++)
+				{
+				    sb.append(Integer.toString((encodedhash[i] & 0xff) + 0x100, 16).substring(1));
+				}
+				hash = sb.toString();
+				
+				/** save the hash to visualledger-file and return the block
+				*/
+				WriteToFile.insert("visualledger", hash);
+				String block = hash + dataToHash;
+				//block sequence is: hash0-64, previousHash65-128,Timestamp129-141,source142-
+				System.out.println(block);
+				return block;
     }
 }
